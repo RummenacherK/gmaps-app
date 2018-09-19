1d4c523d08fd8ccad5fb40d9c80f5b143a329a5d
Andrew Krummenacher <Krummenacher@live.com>
2018-09-19 13:55:12 -0500

    Added destination markers
    Destination markers added using Google maps api

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,14 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def set_destination
+    @destination = Destination.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def destination_params
+    params.require(:destination).permit(:city, :country, :latitude, :longitude)
+  end
+
 end
